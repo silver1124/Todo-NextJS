542ef48a149e06a6af0fbf171c5e9d7f70fd73dd
silver1124 <yashtiwari1124>
2023-09-24 17:24:43 +0530

    Saving the Todo status

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -6,6 +6,7 @@ const TaskList = ({ tasks, onDelete, onToggle }) => {
       {tasks && tasks.map((task) => (
         <Task
           key={task.id}
+          id={task.id}
           task={task}
           onDelete={onDelete}
           onToggle={onToggle}
